d36c849596ee813a3a755bfc01bff1e74522f4d5
BC Ubuntu <balthazar.coquard@gmail.com>
2024-07-07 14:36:41 +0200

    Screenshot

--- function.py
+++ function.py
@@ -10,7 +10,7 @@ def player_guess():
     
     #using while loop in order to repeat the question until the guess is valid. Using not in + list to get/ not different of
     while guess not in ['0','1','2']:
-        guess = input("Pick a number between 0, 1 and 2")
+        guess = input("Pick a number between 0, 1 and 2 :  ")
         
     #return the guess of the player and save it as an integer 
     return int(guess)
